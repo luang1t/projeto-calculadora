bcfc69a72c40c7c4a72828005163f7a65576eefb
Luan Rodrigues <luanzragit@gmail.com>
2025-08-28 00:46:36 -0300

    finalizando tratamento de exceções

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 def soma(float_a:float,float_b:float):
     return float_a + float_b
@@ -19,46 +18,58 @@ def menu():
 def clear():
     return os.system('cls')
 def receber_valores():
-    float_a = float(input("Digite o primeiro valor: "))
-    float_b = float(input("Digite o segundo valor: "))
-    return float_a,float_b
+    try:
+        float_a = float(input("Digite o primeiro valor: "))
+        float_b = float(input("Digite o segundo valor: "))
+        return float_a,float_b
+    except ValueError:
+        print("Digite apenas números reais.")
 
 while True:
-    menu()
     try:
-        seletor = int(input("Digite aqui:"))
-    except ValueError:
-        print("Digite um valor válido.")
-        continue
-    match seletor:
-        case 1:
-            float_a, float_b = receber_valores()
-            resultado = soma(float_a,float_b)
-            clear()
-            print(f"{float_a} + {float_b} = {resultado}")
-            input("Pressione enter para continuar.")
-        case 2:
-            float_a, float_b = receber_valores()
-            resultado = sub(float_a,float_b)
-            clear()
-            print(f"{float_a} - {float_b} = {resultado}")
-            input("Pressione enter para continuar.")
-        case 3:
-            float_a, float_b = receber_valores()
-            resultado = mult(float_a,float_b)
-            clear()
-            print(f"{float_a} x {float_b} = {resultado}")
-            input("Pressione enter para continuar.")
-        case 4:
-            try:
+        menu()
+        try:
+            seletor = int(input("Digite aqui:"))
+        except ValueError:
+            print("Digite um valor válido.")
+            continue
+        match seletor:
+            case 1:
                 float_a, float_b = receber_valores()
-            except ZeroDivisionError:
-                print("Divisão por 0 não é aceita!")
+                resultado = soma(float_a,float_b)
+                clear()
+                print(f"{float_a} + {float_b} = {resultado}")
                 input("Pressione enter para continuar.")
-                continue 
-            resultado = divi(float_a,float_b)
-            clear()
-            print(f"{float_a} / {float_b} = {resultado}")
-            input("Pressione enter para continuar.")        
-        case 0:
-            break       
+            case 2:
+                float_a, float_b = receber_valores()
+                resultado = sub(float_a,float_b)
+                clear()
+                print(f"{float_a} - {float_b} = {resultado}")
+                input("Pressione enter para continuar.")
+            case 3:
+                float_a, float_b = receber_valores()
+                resultado = mult(float_a,float_b)
+                clear()
+                print(f"{float_a} x {float_b} = {resultado}")
+                input("Pressione enter para continuar.")
+            case 4:
+                try:
+                    float_a, float_b = receber_valores()
+                    resultado = divi(float_a,float_b)
+                except ZeroDivisionError:
+                    print("Divisão por 0 não é aceita!")
+                    input("Pressione enter para continuar.")
+                    continue 
+                clear()
+                print(f"{float_a} / {float_b} = {resultado}")
+                input("Pressione enter para continuar.")
+                continue       
+            case 0:
+                break 
+            case _:
+                print("Digite algo válido, no contexto do programa.")
+                input("pressione enter para continuar: ")
+                continue
+    except Exception as e:
+        print(f"Erro inesperado {e}")
+        input("Pressione enter para continuar...")            
